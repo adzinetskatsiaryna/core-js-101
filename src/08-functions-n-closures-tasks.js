d580d6083f2069dfa8f1d8e742ab2db449cc1f17
adzinets <adzinetskatsiaryna@gmail.com>
2022-01-09 14:13:19 +0300

    add solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -62,8 +62,13 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...arg) {
+  return (x) => {
+    if (arg.length === 1) return arg[0];
+    if (arg.length === 2) return x * arg[0] + arg[1];
+    if (arg.length === 3) return x ** 2 * arg[0] + arg[1] * x + arg[2];
+    return null;
+  };
 }
 
 
@@ -147,8 +152,8 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => fn(...args1, ...args2);
 }
 
 
@@ -169,8 +174,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let caunter = startFrom - 1;
+  return () => {
+    caunter += 1;
+    return caunter;
+  };
 }
 
 
